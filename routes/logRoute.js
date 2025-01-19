6b20a63b99cb3b773cb47f69d21373f9f7e54227
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2025-01-19 09:14:53 +0530

    style: align code lines for better readability

--- routes/logRoute.js
+++ routes/logRoute.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { sendMessage } = require('../controllers/logController')
 const router = express.Router()
-// const { sendMessage, getBufferStatus } = require('../services/bufferService')
+
+const { sendMessage } = require('../controllers/logController')
+
 
 router.post(
     '/', 
